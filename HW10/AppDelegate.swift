412ff0a78d3c9184aa71e9db993727dfed73552b
Ryan Milvenan <ryan@warballoon.com>
2016-03-31 17:28:49 -0600

    Initial Commit

---
+++ HW10/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HW10
+//
+//  Created by Wind on 3/31/16.
+//  Copyright © 2016 Ryan Milvenan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HW10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HW10
+//
+//  Created by Wind on 3/31/16.
+//  Copyright © 2016 Ryan Milvenan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
